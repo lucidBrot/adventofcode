d7d0a4b44921c63f3d5cb232f092792a90b2c2a9
U-FRIENDLY_NAME\eric <eric@mink.li>
2018-12-12 16:22:36 +0100

    added adds

--- main.cpp
+++ main.cpp
@@ -7,4 +7,7 @@
 int main(int argc, char* argv[]){
     std::cout << "Hello World" << std::endl;
     TreeNode t = TreeNode(3);
+    t.addMetadata(3);
+    t.addMetadata(4);
+    std::cout << "metadata sum in t: " << t.getMetadataSum() << std::endl;
 }
