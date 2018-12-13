8fdfad502aa73c590f910f1b47b844085c46f710
Eric Mink <eric@mink.li>
2018-12-13 12:39:45 +0100

    compiles

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 using Eigen::MatrixXd;
 #include <sstream>
 #include <string>
+#include <cassert>
 
 enum class Cart {None=0, Up, Down, Left, Right, Crashed};
 
-void parseCartsPositions(std::string filecontents){
+
+MatrixXd parseCartsPositions(std::string filecontents, unsigned int maxX, unsigned int maxY){
+    MatrixXd cartStorage = MatrixXd::Zero(maxY, maxX);
     std::istringstream f(filecontents);
     std::string line;
     unsigned int x = 0;
@@ -19,20 +22,35 @@ void parseCartsPositions(std::string filecontents){
                 case 'v':
                     // down cart
                     cart = Cart::Down;
-                    std::cout << "c is of enum : " << static_cast<int>(cart) << std::endl;
                     break;
+                case '^':
+                    cart = Cart::Up;
+                    break;
+                case '<':
+                    cart = Cart::Left;
+                    break;
+                case '>':
+                    cart = Cart::Right;
+                    break;
+
                 default:
                     // not a cart, but increases coordinate nonetheless
+                    cart = Cart::None;
                     break;
             }
+            cartStorage(y,x) = static_cast<int>(cart);
             x++;
         }
         x=0;
         y++;
     }
+    assert( maxY == y);
+    return cartStorage;
+
 }
 
 int main() { 
+    std::cout << std::endl;
   MatrixXd m(2,2); 
   m(0,0) = 3; 
   m(1,0) = 2.5; 
@@ -40,5 +58,5 @@ int main() {
   m(1,1) = m(1,0) + m(0,1); 
   std::cout << m << std::endl; 
 
-  parseCartsPositions("asdv\nfgv");
+  std::cout << parseCartsPositions("asdv\nfgv>", 4, 2) << std::endl;
 }
