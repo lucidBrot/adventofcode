0b7736a0d07dfe279b0c1feb8c0263162b57fbf8
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-07-24 21:36:36 +0200

    numpy list

--- water.py
+++ water.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from enum import Enum
+import numpy as np
 
 class Ground(Enum):
     CLAY = 0
@@ -33,7 +34,25 @@ class Block:
                 }
         return conductive_grounds[self.ground_type]
 
+class Search:
+    # clay_veins LIKE [{'x': 12, 'y': 3}]
+    def __init__(self, clay_veins : list):
+        # sort by y, then x
+        self.sorted_clay_veins = sorted(clay_veins, key=lambda dic:(dic['y'], dic['x']))
+        self.x_max = self.sorted_clay_veins[-1]['x']
+        self.x_min = self.sorted_clay_veins[0]['x']
+        self.y_max = self.sorted_clay_veins[-1]['y']
+        self.y_min = self.sorted_clay_veins[0]['y']
+        self.clay_map = np.empty((1 + self.y_max - self.y_min, 1 + self.x_max - self.x_min))
+        print(self.clay_map)
+
 if __name__ == '__main__':
     b = Block(Ground.SAND)
+    s = Search(
+                [
+                    {'x': 0, 'y':0},
+                    {'x': 1, 'y':3}
+                ]
+            )
     print(b.is_conductive())
 
