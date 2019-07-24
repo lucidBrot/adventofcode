2f5796453c4e9a474fc5fe53a99fafbf79a8a2e5
U-FRIENDLY_NAME\eric <eric@mink.li>
2019-07-24 21:15:19 +0200

    conductive

--- water.py
+++ water.py
@@ -19,11 +19,21 @@ class Ground(Enum):
                 }
         return render_dict[self]
 
-class Block():
-    def __init__(self):
-        pass
+class Block:
+    def __init__(self, ground_type : Ground):
+        self.ground_type = ground_type
+
+    def is_conductive(self):
+        conductive_grounds = {
+                Ground.CLAY : False,
+                Ground.SAND : True,
+                Ground.SPRING: True,
+                Ground.STILL_WATER: False,
+                Ground.FLOWING_WATER: True,
+                }
+        return conductive_grounds[self.ground_type]
 
 if __name__ == '__main__':
-    g = Ground.CLAY
-    print(g.render())
+    b = Block(Ground.SAND)
+    print(b.is_conductive())
 
