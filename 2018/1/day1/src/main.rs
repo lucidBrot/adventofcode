9da160de3af6641fda173e290dbeb970402948d1
Eric Mink <eric@mink.li>
2019-09-15 22:06:28 +0200

    playing with rust doc generation

--- 2018/1/day1/src/main.rs
+++ 2018/1/day1/src/main.rs
@@ -3,13 +3,16 @@
  * Author: Eric Mink
  */
 
+/// My module *supports* **markdown** in the documentation
 mod main_mod {
+    /// entry point for my hello world module
     pub fn run(){
         println!("Module Ran!");
         let string : String = SampleStruct{val: 5}.sample(3);
         println!("{}", string);
     }
 
+    /// a struct that contains a single `i32` value
     struct SampleStruct {
         val : i32,
     }
@@ -34,7 +37,20 @@ mod main_mod {
     }
 }
 
-fn main() {
+/// # main function documentation  
+/// only public functions are documented
+/// unless generated with
+/// ```bash
+/// cargo rustdoc -- \
+/// --no-defaults \
+/// --passes strip-hidden \
+/// --passes collapse-docs \
+/// --passes unindent-comments \
+/// --passes strip-priv-imports
+/// ```
+/// or more simply:  
+/// `cargo doc --document-private-items`
+pub fn main() {
     println!("Hello, world!");
     main_mod::run();
 }
