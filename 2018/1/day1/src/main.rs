c0d7a951a2ff5ba776b7aa76108c0ad9d5e68758
Eric Mink <eric@mink.li>
2019-09-15 20:39:08 +0200

    compiling hello world

--- 2018/1/day1/src/main.rs
+++ 2018/1/day1/src/main.rs
@@ -1,3 +1,27 @@
+/*
+ * My first attempt with Rust.
+ * Author: Eric Mink
+ */
+
+mod main_mod {
+    pub fn run(){
+        println!("Module Ran!");
+        let string : String = SampleStruct{val: 5}.sample(3);
+        println!("{}", string);
+    }
+
+    struct SampleStruct {
+        val : i32,
+    }
+
+    impl SampleStruct {
+        fn sample(&self, x: i32) -> String {
+            return format!("Hello {}",(x+&self.val));
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    main_mod::run();
 }
