d9bc0ff8ada17b918895af5a69cae2b4620c7beb
Eric Mink <eric@mink.li>
2019-09-17 20:21:29 +0200

    cleaner way, using an output parameter

--- 2018/2/day2/src/main.rs
+++ 2018/2/day2/src/main.rs
@@ -1,46 +1,39 @@
 // macro for parsing strings
 #[macro_use] extern crate scan_rules;
 
-use std::cell::{RefCell,RefMut,Ref};
-
 // define type Reader to learn how to use types
 #[derive(Debug)]
 struct Reader<'a> {
     filename: &'a str,
-    content_rc: RefCell<String>,
 }
 
 // define functionality of reader
 impl Reader<'_> {
 
     fn new(filename: &str) -> Reader {
-        Reader {filename: filename, content_rc: RefCell::new(String::new())}
+        Reader {filename: filename}
     }
 
-    fn read(&self) -> std::io::Result<Ref<String>> {
+    fn read(&self, mut output: &mut String) -> std::io::Result<()> {
         use std::io::prelude::*;
         use std::fs::File;
 
         let mut f = File::open(&self.filename)?;
 
-        {
-        let mut rc_refmut : RefMut<_> = self.content_rc.borrow_mut();
-        (*rc_refmut).clear();
-        f.read_to_string(&mut rc_refmut)?;
-        }
-
-        let myref : Ref<String> = self.content_rc.borrow();
-
-        return Ok(myref);
+        output.clear();
+        f.read_to_string(&mut output)?;
+    
+        return Ok(());
     }
 }
 
 fn main() {
     println!("Hello, world!");
-    let mut reader = Reader::new("input.txt");
+    let reader = Reader::new("input.txt");
     println!("Reader: {:?}", reader);
-    let len = match reader.read() {
-        Ok(s) => s.len(),
+    let mut output : String = String::new();
+    let len = match reader.read(&mut output) {
+        Ok(_) => output.len(),
         Err(e) => { println!("Error parsing file: {:?}", e); 0 },
     };
     println!("File length: {}", len);
