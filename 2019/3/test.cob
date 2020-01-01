11842d8c8817f3868b834bf545ca0b679f580dd0
U-FRIENDLY_NAME\eric <eric@mink.li>
2020-01-01 13:54:57 +0100

    doc comments

--- 2019/3/test.cob
+++ 2019/3/test.cob
@@ -1,7 +1,12 @@
-        IDENTIFICATION DIVISION.
-        PROGRAM-ID. HELLO.
-        AUTHOR. LUCID.
-        DATA DIVISION.
-        PROCEDURE DIVISION.
+      * Required: Info about the program
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+       AUTHOR. LUCID.
+      * INPUT / OUTPUT
+       ENVIRONMENT      DIVISION.
+      * Constants
+       DATA DIVISION.
+      * Executable Code
+       PROCEDURE DIVISION.
            DISPLAY 'Hello World!'.
            STOP RUN.
