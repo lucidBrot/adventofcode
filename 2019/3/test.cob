c3d659a0b7fcc8c494f8c2b6e3d7d1375c6a534b
U-FRIENDLY_NAME\eric <eric@mink.li>
2020-01-01 14:58:04 +0100

    HERBERT THE GREAT

--- 2019/3/test.cob
+++ 2019/3/test.cob
@@ -12,11 +12,41 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
        SELECT MYINPUTFILE ASSIGN TO 'input.txt'
-       ORGANIZATION IS LINE SEQUENTIAL.
-      * Constants
+           ORGANIZATION IS LINE SEQUENTIAL.
+      * Constants and Variables are in the Data Division:
       * File, Working-storage for temp variables, Local-Storage for allocated variables, Linkage
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      * Define a variable at hierarchy 01 (up to 49) or in the case of
+      * 01 it can also be a constant. See page 111 (pdf page 125) for
+      * more info.
+      * Also see
+      * https://open-cobol.sourceforge.io/guides/GnuCOBOL%202.2%20NOV2017%20Programmers%20Guide%20(US%20Letter).pdf
+      * page 173 Section 5.9.24
+      * PIC A(bytecount) is alphabetic, PIC X(bytecount) is alphanumeric
+      * "PIC" seems to stand for "picture-string"
+       01 WS-CONTENT.
+      *    Numeric String of size 5 bytes (zero-initialized)
+           05 WS-SOMETHING PIC 9(5).
+      *    Alphabetic String of size 25 bytes
+           05 WS-SOME-NAME PIC A(25).
+      * Alphabetic string of 1 byte length:
+       01 WS-EOF PIC A(1).
+      * Valid PICTURE characters are A(alphabetic), N(single character -
+      * same as X(2)),
+      * X(Alphanumeric), Z, 1, 9 and *
+      * I don't know yet what all of them do.
+
       * Executable Code
        PROCEDURE DIVISION.
+           DISPLAY WS-SOMETHING.
            DISPLAY 'Hello World!'.
            OPEN Input MYINPUTFILE.
+           PERFORM UNTIL WS-EOF='Y'
+               READ MYINPUTFILE INTO WS-CONTENT
+                   AT END MOVE 'Y' TO WS-EOF
+                   NOT AT END DISPLAY WS-SOME-NAME
+               END-READ
+           END-PERFORM.
+           CLOSE MYINPUTFILE.
            STOP RUN.
