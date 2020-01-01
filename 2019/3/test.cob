162e2e76a56b783001c5c6da0bd1ae2050e7fc94
U-FRIENDLY_NAME\eric <eric@mink.li>
2020-01-01 14:12:03 +0100

    I'd like to read a file

--- 2019/3/test.cob
+++ 2019/3/test.cob
@@ -2,10 +2,18 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. HELLO.
        AUTHOR. LUCID.
-      * INPUT / OUTPUT
-       ENVIRONMENT      DIVISION.
+      * INPUT / OUTPUT and such
+       ENVIRONMENT DIVISION.
+      * Sections are parts of a division:
+       CONFIGURATION SECTION.
+      ** SOURCE-COMPUTER. XXX.
+      ** OBJECT-COMPUTER. XXX
+       INPUT OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT FILENAME 
       * Constants
        DATA DIVISION.
+        * File, Working-storage for temp variables, Local-Storage for allocated variables, Linkage
       * Executable Code
        PROCEDURE DIVISION.
            DISPLAY 'Hello World!'.
