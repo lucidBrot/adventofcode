b866098dd4b986220e7817aa8cd927e4ee81d3ba
U-FRIENDLY_NAME\eric <eric@mink.li>
2020-01-01 13:41:49 +0100

    Hello World

---
+++ 2019/3/test.cob
@@ -0,0 +1,7 @@
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. HELLO.
+        AUTHOR. LUCID.
+        DATA DIVISION.
+        PROCEDURE DIVISION.
+           DISPLAY 'Hello World!'.
+           STOP RUN.
