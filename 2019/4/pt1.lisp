d8e35394ccbb6c2a8cc2a75f9232df83ac678c72
Eric Mink <eric@mink.li>
2020-02-19 12:56:24 +0100

    compute difference

---
+++ 2019/4/pt1.lisp
@@ -0,0 +1,42 @@
+;;; Introductory comments are preceded by ";;;"
+;;; Function headers are preceded by ";;"
+;;; Inline comments are introduced by ";"
+;;;
+
+;;
+;; Triple the value of a number
+;;
+
+(defun triple (X)
+  "Compute three times X."  ; Inline comments can
+  (* 3 X))                  ; be placed here.
+
+;;
+;; Negate the sign of a number
+;;
+
+(defun negate (X)
+  "Negate the value of X."  ; This is a documentation string.
+  (- X))     
+
+;;
+;; Test recursion
+;;
+
+(defun factorial (N)
+  "Compute the factorial of N."
+  (if (= N 1)
+      1
+    (* N (factorial (- N 1)))))
+
+(defun factorial-opt (n &key (result 1))
+    (if (= n 1)
+        result
+        (factorial-opt (- n 1) :result (* result n))))
+
+
+;;; Actual advent of code stuff
+(defun compute (lower_n upper_n)
+  "Compute advent of code day 4 pt 1 given the two input numbers"
+  (- upper_n lower_n)
+  )
