7f7d4419ffe0a543e9609697b386e53c8fbbeaad
Eric Mink <eric@mink.li>
2020-02-28 21:59:25 +0100

    construct an object

--- 2019/5/five/index.php
+++ 2019/5/five/index.php
@@ -1,3 +1,24 @@
 <?php
+class IntCodeProgram {
+    public $commands;
+}
+
+class Intstruction {
+    public $raw; // all digits, split on commas
+
+    function get_size(){
+        return count($this->raw, COUNT_NORMAL);
+    }
+
+    function __construct($raw) {
+        $this->raw = $raw;
+    }
+}
+
+$inst_1 = new Intstruction(array(1,2,3,0));
+var_dump($inst_1);
 ?>
-	Something is right with the XAMPP installation :-(
+
+</br><div>
+    Something is right with the XAMPP installation :-(
+</div>
