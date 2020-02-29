03147bfb026fcbf3f9ac4882776b02ae5c62233a
Eric Mink <eric@mink.li>
2020-02-29 12:39:56 +0100

    count args

--- 2019/5/five/index.php
+++ 2019/5/five/index.php
@@ -1,28 +1,51 @@
 <?php
-class IntCodeProgram {
-    public $commands;
+function instr_num_input_args($opcode) {
+    switch ($opcode) {
+        case 01:
+            return 2;
+        case 02:
+            return 2;
+        case 03:
+            return 0;
+        case 04:
+            return 1;
+        case 99:
+            return 0;
+        default: // shouldn't happen
+            return 0;
+    }
 }
 
-class Intstruction {
-    public $raw; // all digits, split on commas
-
-    function get_size(){
-        return count($this->raw, COUNT_NORMAL);
-    }
-
-    function __construct($raw) {
-        $this->raw = $raw;
+function instr_num_output_args($opcode) {
+    switch ($opcode) {
+        case 01:
+            return 1;
+        case 02:
+            return 1;
+        case 03:
+            return 1;
+        case 04:
+            return 0;
+        case 99:
+            return 0;
+        default: // shouldn't happen
+            return 0;
     }
 }
 
+function instr_num_args($opcode) {
+    return instr_num_input_args($opcode) + instr_num_output_args($opcode);
+}
 
-$inst_1 = new Intstruction(array(1,2,3,0));
-var_dump($inst_1);
-
+// read input into int array
 $inputfile = fopen("./input.txt", "r");
 $inputarr = fgetcsv($inputfile);
 $intputarr = array_map('intval', $inputarr);
-var_dump($intputarr);
+
+// test with first instruction
+$instr = $intputarr[0];
+$lenn = instr_num_args($instr);
+echo("instruction: ".$instr."  numArgs: ".$lenn);
 ?>
 
 </br><div>
