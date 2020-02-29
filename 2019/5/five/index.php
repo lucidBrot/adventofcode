2a43f15d344430bd8ffcbf67118f73e62167b75b
Eric Mink <eric@mink.li>
2020-02-29 13:24:59 +0100

    how to stop?

--- 2019/5/five/index.php
+++ 2019/5/five/index.php
@@ -37,6 +37,65 @@ function instr_num_args($opcode) {
     return instr_num_input_args($opcode) + instr_num_output_args($opcode);
 }
 
+class IntComputer {
+    protected $memory;
+
+    function __construct($memory){
+        $this->memory = $memory;
+    }
+
+    function get_value($location_or_value, $accessMode){
+        switch ($accessMode) {
+        case 00:
+            return $this->$memory[$location_or_value];
+        case 01:
+            return $location_or_value;
+        }
+    }
+
+    function perform_instruction($opcode, $accessModes, ...$args) {
+        // get input args
+        $no = instr_num_output_args($opcode);
+        $ni = instr_num_input_args($opcode);
+        $inputargs = array_slice($args, 0, $ni);
+        $outputargs = array_slice($args, $ni, $no);
+
+        // pad accessModes with leading zeros
+        $acc = array_pad($accessModes, -1 * $n, 0);
+
+        // get input argument values
+        $vals = [];
+        for ($i = 0; $i < $ni; $i++){
+            $vals += $this->get_value($inputargs[$i], $acc[$i]);
+        }
+
+        // combine arguments
+        $valargs = $vals + $outputargs;
+
+        // call the relevant execution
+        switch ($opcode) {
+            // the output arguments are, by the specs, always locations and not immediate values.
+        case 01:
+            perform_add(...$valargs);
+            break;
+        case 02:
+            perform_multiply(...$valargs);
+            break;
+        case 03:
+            perform_store_input($outputargs[0]);
+            break;
+        case 04:
+            perform_output($inputargs[0])
+        case 99:
+            perform_exit(); // TODO: how to stop?
+            break;
+
+            
+
+        }
+    }
+}
+
 // read input into int array
 $inputfile = fopen("./input.txt", "r");
 $inputarr = fgetcsv($inputfile);
@@ -46,6 +105,9 @@ $intputarr = array_map('intval', $inputarr);
 $instr = $intputarr[0];
 $lenn = instr_num_args($instr);
 echo("instruction: ".$instr."  numArgs: ".$lenn);
+
+// do actual thing
+$comp = new IntComputer($intputarr);
 ?>
 
 </br><div>
