99964ada9d6c568a2fb7d37f0e23d8d5122c3ae3
Eric Mink <eric@mink.li>
2020-03-01 19:35:40 +0100

    work on pt 2

--- 2019/5/five/index.php
+++ 2019/5/five/index.php
@@ -1,4 +1,7 @@
 <?php
+$PART_ONE_INPUT = 1;
+$PART_TWO_INPUT = 5;
+
 function instr_num_input_args($opcode) {
     switch ($opcode) {
     case 01:
@@ -11,6 +14,14 @@ function instr_num_input_args($opcode) {
         return 1;
     case 99:
         return 0;
+    case 05:
+        return 2;
+    case 06:
+        return 2;
+    case 07:
+        return 2;
+    case 8:
+        return 2;
     default: // shouldn't happen
         return 0;
     }
@@ -28,6 +39,14 @@ function instr_num_output_args($opcode) {
         return 0;
     case 99:
         return 0;
+    case 05:
+        return 0;
+    case 06:
+        return 0;
+    case 07:
+        return 1;
+    case 8:
+        return 1;
     default: // shouldn't happen
         return 0;
     }
@@ -40,6 +59,7 @@ function instr_num_args($opcode) {
 class IntComputer {
     protected $memory;
     protected $program_ended = false;
+    protected $pc;
 
     function __construct($memory){
         $this->memory = $memory;
@@ -65,11 +85,11 @@ class IntComputer {
     }
 
     function run(){
-        $pc = 0;
+        $this->pc = 0;
         $this->program_ended=False;
         while (! $this->program_ended ){
             // 2-digit opcode, leading digits are accessModes
-            $opcodeWithAccessModesAsNumber = $this->memory[$pc];
+            $opcodeWithAccessModesAsNumber = $this->memory[$this->pc];
             echo("opcodeWithAccessModesAsNumber: ".$opcodeWithAccessModesAsNumber."<br/>");
             // turn this into an array and an opcode
             $opcode = $opcodeWithAccessModesAsNumber % 100;
@@ -79,12 +99,12 @@ class IntComputer {
             $n = instr_num_args($opcode);
 
             // execute instruction
-            $args = array_slice($this->memory, $pc + 1, $n);
+            $args = array_slice($this->memory, $this->pc + 1, $n);
             echo("About to execute ".$opcode." with access modes ".implode(" ", $accessModes)." and arguments ".implode(" ", $args)."<br/>");
             $this->perform_instruction($opcode, $accessModes, ...$args);
 
             // increase program counter
-            $pc = $pc + 1 + $n;
+            $this->pc = $this->pc + 1 + $n;
         }
 
     }
@@ -136,6 +156,18 @@ class IntComputer {
         case 04:
             $this->perform_output($valargs[0]);
             break;
+        case 05:
+            $this->perform_jnz(...$valargs);
+            break;
+        case 06:
+            $this->perform_jz(...$valargs);
+            break;
+        case 07:
+            $this->perform_less_than(...$valargs);
+            break;
+        case 8:
+            $this->perform_equals(...$valargs);
+            break;
         case 99:
         default:
             $this->perform_exit();
@@ -157,12 +189,35 @@ class IntComputer {
     }
  
     function perform_store_input($target){
-        // TODO: actually get input instead of hardcoded 1?
-        $this->set_value($target, 1);
+        global $PART_ONE_INPUT;
+        global $PART_TWO_INPUT;
+        $this->set_value($target, $PART_TWO_INPUT);
     }
 
     function perform_output($something){
         echo("output:: ".strval($something)."<br/>");
+    }
+
+    function perform_jnz($conditional, $target_pc){
+        // the pc will be increased before the next instruction, so we set it one lower than told to
+        if($conditional != 0) {
+            $this->pc = $target_pc -1;
+        }
+    }
+
+    function perform_jz($conditional, $target_pc){
+        if($conditional == 0){
+            $this->pc = $target_pc;
+        }
+    }
+
+    function perform_less_than($sm, $lg, $target){
+        $v = $sm < $lg ? 1 : 0;
+        $this->set_value($target, $v);
+    }
+
+    function perform_equals($a, $b, $target){
+        $this->set_value($target, $a == $b ? 1 : 0);
     }
 }
 
