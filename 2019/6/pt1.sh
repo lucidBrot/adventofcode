70825ff8f914677c59cbc090672707ab630eed0f
Eric Mink <eric@mink.li>
2020-04-11 01:32:35 +0200

    in between

--- 2019/6/pt1.sh
+++ 2019/6/pt1.sh
@@ -13,11 +13,41 @@ unset current_orbitees["ASD"]
 declare -A next_orbitees
 # an empty string means it does not exist. But it does show up in the length entry.
 next_orbitees["ASD"]=""
-unset current_orbitees["ASD"]
+unset next_orbitees["ASD"]
 
 orbits_counter=0
 step=1
 
-current_orbitee_set_size=${#current_orbitees[@]}
+$INP="ASD)FGH\nCOM)ASD"
+# alternatively
+# $INP=`cat input1.txt`
+# Store input in an array, line by line
+# https://stackoverflow.com/a/918931/2550406
+IFS='\n' read -ra IN <<< "$INP"
 
-echo $current_orbitee_set_size
+# generator that loops the input
+linenr=-1
+function next_inp_line {
+    linenr=$(( ( 1 + linenr ) % ${#current_orbitee[@]} ))
+    echo ${IN[$linenr]}
+}
+
+# while current_orbitee_set_size greater 0
+while [ ${#current_orbitee[@]} -gt 0 ]
+do
+    local num_satellites=0
+    unset next_orbitees
+    declare -A next_orbitees
+
+    # make sure we get the line N times before we check again whether the set is empty
+    # get next line
+    local line="$(next_inp_line)"
+    # split the line into orbitee and satellite
+    IFS=';' read -ra ORBIT <<< "$line"
+
+    
+
+    orbits_counter=$(( orbits_counter + num_satellites*step ))
+    step=$(( step + 1 ))
+    current_orbitees=$next_orbitees
+done
