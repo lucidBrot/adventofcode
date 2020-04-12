a1be284b31306ca6cbcb76ab92aa7a8639d0c19d
Eric Mink <eric@mink.li>
2020-04-12 14:26:19 +0200

    small example works

--- 2019/6/pt2.sh
+++ 2019/6/pt2.sh
@@ -14,10 +14,12 @@ entries_matching () {
     if [[ ${your_orbitees[$latest_santa]} ]]
     then
         echo "$latest_santa"
+        return 0
     fi
     if [[ ${santas_orbitees[$latest_you]} ]]
     then
         echo "$latest_you"
+        return 0
     fi
     echo ""
 }
@@ -31,22 +33,29 @@ santas_orbitee='SAN'
 your_orbitee='YOU'
 while
     # do everything. TODO: only do that if not COM
-    santas_orbitee="$(grep ')'$santas_orbitee input2.txt | sed -e 's/).*$//g')"
-    your_orbitee="$(grep ')'$your_orbitee input2.txt | sed -e 's/).*$//g')"
     step=$((1 + step))
-    # store the step number for easy retrieval later
-    santas_orbitees[$santas_orbitee]=$step
-    your_orbitees["$your_orbitee"]=$step
+    if [[ $santas_orbitee != 'COM' ]]
+    then
+        santas_orbitee="$(grep ')'$santas_orbitee input2.txt | sed -e 's/).*$//g')"
+        # store the step number for easy retrieval later
+        santas_orbitees[$santas_orbitee]=$step
+    fi
+    if [[ $your_orbitee != 'COM' ]]
+    then
+        your_orbitee="$(grep ')'$your_orbitee input2.txt | sed -e 's/).*$//g')"
+        # store the step number for easy retrieval later
+        your_orbitees["$your_orbitee"]=$step
+    fi
 
     match="$(entries_matching "$santas_orbitee" "$your_orbitee")"
     empty=''
 
     # as long as this check holds
-    (( "match" == empty ))
+    [[ $match == $empty ]]
 do
     continue
 done
 
 # subtract the steps SAN->A, YOU->B and the duplicate count of the intersection
-num_steps_needed=$(( ${santas_orbitees[$match]} + ${your_orbitees[$match]} - 3 ))
+num_steps_needed=$(( ${santas_orbitees[$match]} + ${your_orbitees[$match]} - 2 ))
 echo "$num_steps_needed"
