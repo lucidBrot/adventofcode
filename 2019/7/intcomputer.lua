e0734dceacaeb936cb5466644928cdac628c49e7
Eric Mink <eric@mink.li>
2020-04-13 22:42:24 +0200

    ah wait. memory starts from 0...

--- 2019/7/intcomputer.lua
+++ 2019/7/intcomputer.lua
@@ -32,15 +32,23 @@ function M.instr_num_args(opcode)
     return M.instr_num_input_args[opcode] + M.instr_num_output_args[opcode]
 end
 
+-- ternary operator because the common
+--   x = a and b or c
+-- is spooky when b evaluates to false (and would have to be swapped then)
+function T(condition, yes, no)
+    if condition then return yes else return no end
+end
+
 -- a "class"
 IntComputer = {}
 
     function IntComputer:new(phase_setting, mem)
+        local m = T(mem == nil, {}, mem)
         newObj = { 
             phase = phase_setting,           -- user-specified ∈[0,4]
             program_ended = false,
             pc = nil,                        -- program counter
-            memory = mem,                    -- array that stores memory
+            memory = m,                    -- array that stores memory
         }
         self.__index = self
         return setmetatable(newObj, self)
@@ -63,29 +71,35 @@ IntComputer = {}
         self.pc = 1
         self.program_ended = false
         repeat
+            print(" ")
             -- 2-digit opcode, leading digits are accessModes
-            opcodeWithAccessModesAsNumber = self.memory[self.pc]
+            local opcodeWithAccessModesAsNumber = self.memory[self.pc]
             -- turn this into a string and an opcode
             opcode = opcodeWithAccessModesAsNumber % 100
-            accessModes = tostring(opcodeWithAccessModesAsNumber / 100)
+            print("<amp" .. self.phase .. "> opcode as Number: " .. opcode)
+            accessModes = tostring(math.floor(opcodeWithAccessModesAsNumber / 100))
             -- the access modes can be fetched using tonumber(accessModes:sub(i,i)) for the ith position
             n = M.instr_num_args(opcode)
             
             -- execute instruction
-            args = { table.unpack(self.memory, self.pc + 1, self.pc + n) } -- TODO: if something is off, the bug might be in this line
-            print("<amp" .. self.phase .. "> about to execute " .. opcode .. " with access modes " .. accessModes .. " and arguments " .. table.unpack(args) .. ".")
+            local args = { table.unpack(self.memory, self.pc + 1, self.pc + n) } -- TODO: if something is off, the bug might be in this line
+            -- print("<amp" .. self.phase .. "> about to execute opcode " .. opcode .. " with access modes " .. accessModes .. " and arguments " .. table.concat(args,", ") .. ".")
+            for a=1, #args do
+                print("    (arg): " .. T(args[a], args[a], "NIL"))
+            end
             self:perform_instruction(opcode, accessModes, args)
 
             -- increment program counter
-            self.pc = self.pc + 1
+            self.pc = self.pc + 1 + n
         until self.program_ended
     end
 
     function IntComputer:perform_instruction(opcode, accessModes, args)
         -- get input args
-        local no = M.instr_num_output_args(opcode)
-        local ni = M.instr_num_input_args(opcode)
+        local no = M.instr_num_output_args[opcode]
+        local ni = M.instr_num_input_args[opcode]
         local n = ni + no
+        assert(n == #args, "Wrong number of arguments.")
         -- TODO: if something is off, the bug might be in the below two lines
         local inputargs = { table.unpack(args, 1, ni) }
         local outputargs = { table.unpack(args, no) }
@@ -100,13 +114,25 @@ IntComputer = {}
         local vals = {}
         for i = 1, ni do
             local acci = tonumber(acc:sub(i,i))
-            print("  loading inputarg " .. inputargs[i] .. " for accessor " .. acci )
             vals[i] = self:get_value(inputargs[i], acci)
+            print("  loading inputarg " .. inputargs[i] .. " for accessor " .. acci .. " : that is " .. tostring(vals[i]) )
         end
 
         -- output arguments are always locations
 
         -- combine arguments
+        local valargs = nil
+        if #outputargs > 0 then
+            valargs = {table.unpack(vals)} -- copy
+            for i = 1, #outputargs do
+                valargs[#vals+i] = outputargs[i]
+            end
+        else
+            valargs = {table.unpack(vals)} -- copy
+        end
+
+        print("<pc:" .. self.pc .. "> [" .. opcode .. "]" .. table.unpack(valargs))
+
     end
 
 -- end IntComputer "class"
--- 2019/7/pt1.lua
+++ 2019/7/pt1.lua
@@ -7,3 +7,33 @@ print('intcomputer: ' .. b.phase)
 local c = tostring(133700 / 100)
 print('num: ' .. c:sub(3,4))
 
+local x = {}
+x[1] = "hello"
+x[2] = "bye"
+local y = x
+y[2] = "rekt"
+print("x2: " .. x[2] ..", y2: " .. y[2])
+
+function parse_comma_str(str)
+    local tab = {}
+    -- match everything but the comma
+    for word in string.gmatch(str, '([^,]+)') do
+        tab[#tab+1] = word
+    end
+    return tab
+end
+
+function read_file(file)
+    local f = assert(io.open(file, "rb"))
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
+function run_file(file)
+    local f = read_file(file)
+    local a = IntComputer:new(1, parse_comma_str(f))
+    a:run()
+end
+
+run_file("input.txt")
