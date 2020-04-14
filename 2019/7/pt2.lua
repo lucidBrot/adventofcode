7bba824a6aecd3551ca84b72c91387ce6703ab18
Eric Mink <eric@mink.li>
2020-04-14 23:46:53 +0200

    why do you want user input?

--- 2019/7/pt2.lua
+++ 2019/7/pt2.lua
@@ -34,13 +34,21 @@ end
 -- for any permutation of [0,1,2,3,4] run the sequence and keep the max
 local MAX_result = nil
 function callback(phases)
-    local next_input = {0}
     -- create all coroutines in a suspended state
     local coroutines = {}
+    local bots = {}
+    local f = Q.parse_comma_str(Q.read_file("input_7.txt"))
+    -- first bot
+    bots[1] = IntComputer:new(phases[1], f, {0}, true)
+    -- other bots
+    for i = 2, #phases do
+        -- no input specified before we get some output
+        bots[i] = IntComputer:new(phases[i], f, {}, true)
+    end
+    -- coroutines
     for i = 1, #phases do
-        -- I'm assuming the next input is always just one number
-        coroutines[i] = coroutine.create(function () Q.run_file("input_7.txt", phases[i], next_input ) end )
-        next_input = {}
+        local phase_nr = i
+        coroutines[i] = coroutine.create(function () bots[phase_nr]:run() end )
     end
 
     -- run the first one, until it outputs a value. Then hand that value over to the second one, and so on.
@@ -48,27 +56,32 @@ function callback(phases)
     local retcode = nil -- true unless the coroutine has stopped
     local retval = nil  -- the value that is outputted
     local repeat_outer = true
+    local latest_output_that_was_not_nil = nil
     repeat 
         for i = 1, #coroutines do
             retcode, retval = coroutine.resume(coroutines[i])
-            if retcode == false then
+            if retcode == false or retval == nil then
                 -- break the outer loop
                 repeat_outer = false
                 break
             else
                 -- we have a value in retval
                 -- and we want to pass it into the next coroutine
-                INTCOMP[i+1]:append_inputs({retval})
+                print("result from bot" .. i .. ": " .. retval)
+                local latest_output_that_was_not_nil = retval
+                local next_bot_nr = T(i == #coroutines, 1, i+1)
+                bots[next_bot_nr]:append_inputs({retval})
             end
         end
     until repeat_outer == false
 
-    if MAX_result == nil or MAX_result < retval then
-        MAX_result = retval
+    if MAX_result == nil or MAX_result < latest_output_that_was_not_nil then
+        MAX_result = latest_output_that_was_not_nil
+        print("New possible max found: " .. MAX_result)
     end
 end
 
-local possible_phases = {0,1,2,3,4}
+local possible_phases = {5,6,7,8,9}
 rosetta.permutation(possible_phases, #possible_phases, callback)
 print("MAX: " .. MAX_result)
 
