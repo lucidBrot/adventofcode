71226f3149d300f30314ce67cbde1963e0f2480a
LucidBrot <eric@mink.li>
2023-11-15 15:45:19 +0100

    day1 done

--- 2020/01/pt1.cpp
+++ 2020/01/pt1.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <numeric>
+#include <cassert>
 
 /*
  * Inputs are the calories each elf is carrying.
@@ -40,13 +42,14 @@ class Backpack {
         }
 };
 
-int solve_pt1(std::string input_file){
+int solve_pt1(std::string input_file, bool is_pt2 = false){
     /*
      * I know i am solving this overcomplicatedly and
      * suboptimally in memory consumption. But I want to 
      * freshen up my cpp object oriented skills.
      */
-    std::ifstream input(input_file);
+    std::ifstream input(input_file); 
+    if (input.fail()) {std::cout << "sorry." << std::endl; exit(1);}
     std::vector<Backpack> all_elves;
     std::string line; int index = 0; 
     std::vector<int> current_calories;
@@ -60,17 +63,37 @@ int solve_pt1(std::string input_file){
             // copies the vector elements, when it is called by emplace_back.
             current_calories.clear();
             index += 1;
+            std::cout << "Elf: " << all_elves.back().sum_calories() << "\n" <<std::endl;
         } else {
             int cal; std::istringstream(line) >> cal;
             current_calories.push_back(cal);
         }
     }
+    // end current elf.
+    all_elves.emplace_back(index, current_calories);
 
-    Backpack max_elf = *std::max_element(all_elves.begin(), all_elves.end(), 
-             [](Backpack& a, Backpack& b) { return (a.sum_calories() < b.sum_calories()); }
-            );
+    if (!is_pt2){
+        Backpack max_elf = *std::max_element(all_elves.begin(), all_elves.end(), 
+                [](Backpack& a, Backpack& b) { return (a.sum_calories() < b.sum_calories()); }
+                );
+        return max_elf.sum_calories();
+    } else {
+        assert(all_elves.size() >2);
+        std::nth_element(all_elves.begin(), all_elves.begin()+2, all_elves.end(),
+                // nth_element gets the minimum, so i invert the comparator here because
+                // I want to get the maximum.
+                [](Backpack& a, Backpack& b) { return (a.sum_calories() > b.sum_calories()); }
+                );
+        int total = 0;
+        std::for_each(all_elves.begin(),
+                      all_elves.begin()+3,
+                     [&total](Backpack& b){
+                        std::cout << b.sum_calories() << std::endl;
+                        total += b.sum_calories();
+                });
+        return total;
+    }
     
-    return max_elf.sum_calories();
 }
 
 int main( int argc, char** argv ){
@@ -88,5 +111,10 @@ int main( int argc, char** argv ){
     if (std::string(argv[1]) == "pt1"){
         int solution1 = solve_pt1(std::string(argv[2]));
         std::cout << "Solution Pt1: " << solution1 << std::endl;
+    }
+
+    if (std::string(argv[1]) == "pt2"){
+        int solution1 = solve_pt1(std::string(argv[2]), true);
+        std::cout << "Solution Pt2: " << solution1 << std::endl;
     }
 }
